9d66e27ac8d3aaa8c5ada745f71c08db7d3d5abd
prakashranjan <prakashranjansingh04@gmail.com>
2018-08-13 02:59:24 +0530

    lshift consecutive 1 in binary

---
+++ lshift.py
@@ -0,0 +1,27 @@
+# Python program to find
+# length of the longest
+# consecutive 1s in
+# binary reprsentation of
+# a number.
+ 
+def maxConsecutiveOnes(x):
+ 
+    # Initialize result
+    count = 0
+  
+    # Count the number of iterations to
+    # reach x = 0.
+    while (x!=0):
+     
+        # This operation reduces length
+        # of every sequence of 1s by one.
+        x = (x & (x << 1))
+  
+        count=count+1
+     
+    return count
+ 
+# Driver code
+ 
+print(maxConsecutiveOnes(14))
+print(maxConsecutiveOnes(222))
